c4f24b6bd70d7147155d22b8842b286aa55f2cfb
mao12312 <mao12312dev@gmail.com>
2020-03-16 20:45:00 +0900

    Tweak test.js

--- test.js
+++ test.js
@@ -1,22 +1,25 @@
 'use strict';
-const todo = require('./index.js');
+const fs = require('fs');
 const assert = require('assert');
+fs.unlink('./tasks.json', (err) => {
+    const todo = require('./index.js')
 
-// todo and list test
-todo.todo('task1');
-todo.todo('task2');
-assert.deepEqual(todo.list(), ['task1', 'task2']);
+    // todo and list test
+    todo.todo('task1');
+    todo.todo('task2');
+    assert.deepEqual(todo.list(), ['task1', 'task2']);
 
 
-// done and donelist test
-todo.done('task2');
-assert.deepEqual(todo.list(), ['task1']);
-assert.deepEqual(todo.doneList(), ['task2']);
+    // done and donelist test
+    todo.done('task2');
+    assert.deepEqual(todo.list(), ['task1']);
+    assert.deepEqual(todo.doneList(), ['task2']);
 
-//del test
-todo.del('task1');
-todo.del('task2');
-assert.deepEqual(todo.list(), [])
-assert.deepEqual(todo.doneList(), [])
+    //del test
+    todo.del('task1');
+    todo.del('task2');
+    assert.deepEqual(todo.list(), [])
+    assert.deepEqual(todo.doneList(), [])
 
-console.log('success!');
+    console.log('success!');
+});
